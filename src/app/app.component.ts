845029e0ea235cc09987fc5597b7a4fea4963389
Michal Klik <1456289+TroodoNmike@users.noreply.github.com>
2021-10-09 21:18:22 +0200

    footer and tidy up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CardInterface } from './interfaces/card.interface';
 import { ProfileInterface } from './interfaces/profile.interface';
 import { NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
+import { defaultProfile } from './app.data';
 
 @Component({
     selector: 'app-root',
@@ -9,55 +10,7 @@ import { NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    profiles: ProfileInterface[] = [
-        {
-            name: 'Default Set',
-            cards: [
-                {
-                    id: 0,
-                    address: '1EY2jSam6Q5Kp7D1sbBMhvX8z74Qmkkeuc',
-                    name: 'Account 1',
-                    blockchain: 'bitcoin',
-                    icon: 'btc',
-                    transactions: [],
-                    series: [],
-                    total1: 0,
-                    total5: 0,
-                    total10: 0,
-                    highest: 0,
-                    lowest: 0,
-                },
-                {
-                    id: 1,
-                    address: '0xe055248df087faba7758055cbdf6aa9acf246627',
-                    name: 'Account 2',
-                    blockchain: 'ethereum',
-                    icon: 'eth',
-                    transactions: [],
-                    series: [],
-                    total1: 0,
-                    total5: 0,
-                    total10: 0,
-                    highest: 0,
-                    lowest: 0,
-                },
-                {
-                    id: 2,
-                    address: '9K8QMqQkuCxUFHFh2JogHNEtXSestisnXNdzDwPeDECU',
-                    name: 'Account 3',
-                    blockchain: 'solana',
-                    icon: 'sol',
-                    transactions: [],
-                    series: [],
-                    total1: 0,
-                    total5: 0,
-                    total10: 0,
-                    highest: 0,
-                    lowest: 0,
-                },
-            ],
-        },
-    ];
+    profiles: ProfileInterface[] = [defaultProfile];
     selectedProfile: ProfileInterface = this.profiles[0];
 
     constructor(private tooltipConfig: NgbTooltipConfig) {
@@ -70,8 +23,10 @@ export class AppComponent {
     }
 
     addProfile() {
-        const name = 'Profile ' + this.profiles.length;
-        this.profiles.push({ name: name, cards: [] });
+        this.profiles.push({
+            name: `Profile ${this.profiles.length}`,
+            cards: [],
+        });
         this.selectedProfile = this.profiles[this.profiles.length - 1];
     }
 
@@ -84,11 +39,18 @@ export class AppComponent {
     }
 
     removeProfile() {
-        if (this.selectedProfile != this.profiles[0]) {
-            this.profiles = this.profiles.filter(
-                (t) => t !== this.selectedProfile
-            );
-            this.selectedProfile = this.profiles[0];
+        const confirm = prompt('sure?');
+
+        if (confirm) {
+            // to prevent dropdown being stuck on the screen
+            setTimeout(() => {
+                if (this.selectedProfile != this.profiles[0]) {
+                    this.profiles = this.profiles.filter(
+                        (t) => t !== this.selectedProfile
+                    );
+                    this.selectedProfile = this.profiles[0];
+                }
+            });
         }
     }
 
